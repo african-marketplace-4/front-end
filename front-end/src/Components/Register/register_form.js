9b0ff3da1cce6ca3b7bd0282a1f8587cd4f78536
David Fletcher <davidfletcher@Davids-MacBook-Air-2.local>
2021-09-21 18:28:17 -0400

    added most form, need to add the rest of axios.post

---
+++ front-end/src/Components/Register/register_form.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Form(props){
+    
+    return(
+        <form className="register">
+          <div className="register-comp"> 
+            <label className="name"> First Name: 
+               <input
+                 type="text"
+                 name="user.name.first"
+                 placeholder="First Name"
+                 value={props.user.name.first}
+                />
+            </label>
+            <label className="name"> Last Name: 
+               <input
+                 type="text"
+                 name="name.last"
+                 placeholder="this name will be shared with your peers"
+                 value={props.user.name.last}
+                />
+            </label>
+            <label className="username"> Username: 
+               <input
+                 type="text"
+                 name="username"
+                 placeholder="this name will be shared with your peers"
+                 value={props.user.login.username}
+                />
+            </label>
+            <label> Password: 
+               <input
+                 type="text"
+                 name="password"
+                 placeholder="this will remain confidential"
+                 value={props.user.login.password}
+                />
+            </label>
+
+          </div>
+        </form>
+    )
+}
+export default Form;
