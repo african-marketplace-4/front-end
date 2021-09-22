ffdb1af938ebf77851fa46f8110695379da249c7
David Fletcher <davidfletcher@Davids-MacBook-Air-2.local>
2021-09-21 21:16:31 -0400

    working on form more

--- front-end/src/Components/Register/register.js
+++ front-end/src/Components/Register/register.js
@@ -2,15 +2,21 @@
 
 import React from "react";
 
-//import RegistForm from "./register_form";
+import RegistForm from "./register_form";
 //import { Route } from 'react-router-dom';
 
 
 function Register(props) {
+    const { values, submit, rewrite } = props;
+
     return(
         <>
         <h1> register here </h1>
-            {/* <RegistForm /> */}
+            <RegistForm 
+                values={values}
+                submit={makeNewUser}
+
+                /> 
         </>
     )
 }
--- front-end/src/Components/Register/register_form.js
+++ front-end/src/Components/Register/register_form.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Form(props){
-    
+  
+
+
     return(
         <form className="register">
           <div className="register-comp"> 
